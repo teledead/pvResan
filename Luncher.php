c3c7da90c04b591fe8eddeb09c9e43f72cbaad07
teledead <tele.dead@gmail.com>
2016-08-11 15:21:48 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '**268290275:AAErEBkMkkV07HhI0ll3ENtnqm7SVwo4ixA**');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
